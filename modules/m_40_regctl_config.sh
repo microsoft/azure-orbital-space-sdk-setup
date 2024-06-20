2e170074614cb6aecbccba45f9898646eb3872cb
Ryan Campbell <rycampbe@microsoft.com>
2024-06-20 15:22:06 +0000

    increasing concurrent requests to local registry

--- modules/m_40_regctl_config.sh
+++ modules/m_40_regctl_config.sh
@@ -6,8 +6,8 @@
 function _update_regctl_config_host() {
 
     # Gotta run it twice because regctl keeps sudo config in a difference spot
-    run_a_script "regctl registry set localhost:5000 --tls enabled --req-concurrent 3 --req-per-sec 15 --skip-check" --disable_log
-    run_a_script "regctl registry set localhost:5000 --tls enabled --req-concurrent 3 --req-per-sec 15 --skip-check" --no_sudo --disable_log
+    run_a_script "regctl registry set localhost:5000 --tls enabled --req-concurrent 15 --req-per-sec 15 --skip-check" --disable_log
+    run_a_script "regctl registry set localhost:5000 --tls enabled --req-concurrent 15 --req-per-sec 15 --skip-check" --no_sudo --disable_log
 
     # Gotta run it twice because regctl keeps sudo config in a difference spot
     run_a_script "regctl registry set registry.spacefx.local --tls enabled --req-concurrent 15 --req-per-sec 15 --hostname localhost:5000 --skip-check" --disable_log
@@ -19,6 +19,6 @@ function _update_regctl_config_host() {
 ############################################################
 function _update_regctl_config_devcontainer() {
     # Gotta run it twice because regctl keeps sudo config in a difference spot
-    run_a_script "regctl registry set registry.spacefx.local --tls enabled --req-concurrent 3 --req-per-sec 15 --hostname coresvc-registry.coresvc.svc.cluster.local:5000 --skip-check" --disable_log
-    run_a_script "regctl registry set registry.spacefx.local --tls enabled --req-concurrent 3 --req-per-sec 15 --hostname coresvc-registry.coresvc.svc.cluster.local:5000 --skip-check"  --no_sudo --disable_log
+    run_a_script "regctl registry set registry.spacefx.local --tls enabled --req-concurrent 15 --req-per-sec 15 --hostname coresvc-registry.coresvc.svc.cluster.local:5000 --skip-check" --disable_log
+    run_a_script "regctl registry set registry.spacefx.local --tls enabled --req-concurrent 15 --req-per-sec 15 --hostname coresvc-registry.coresvc.svc.cluster.local:5000 --skip-check"  --no_sudo --disable_log
 }
